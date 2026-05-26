0d08714743b93bcbbd2493a6d1ea0f0585522c21
Tanasi Robert <attacytb@gmail.com>
2026-05-26 22:43:14 +0300

    Added master appid list

---
+++ part1-data-collection/getappids.py
@@ -0,0 +1,31 @@
+# This script fetches the master list of appIDs from the steam
+# store in theory. In practice, the api is down so i downloaded
+# a list of appids that i then worked with. 
+# Credits to jsnli on github for the json file.
+import requests
+import json
+import time
+
+def fetch_game_ids():
+    url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+
+    try:
+        response = requests.get(url)
+
+        # Check for successful call
+        if response.status_code == 200:
+            # Convert bulk data to json
+            json_data = response.json()
+
+            with open("master_list.json", "w", encoding="utf-8") as file:
+                json.dump(json_data, file, indent=4)
+            
+            print("Successfully created bulk list to 'steam_bulk_list.json'!")
+
+        else:
+            print(f"Failed to fetch data, status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occured: {e}")
+        return
+
+fetch_game_ids()
